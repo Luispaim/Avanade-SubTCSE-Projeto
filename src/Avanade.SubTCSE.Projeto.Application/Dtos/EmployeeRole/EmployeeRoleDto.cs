fad0886597b08aca9930abb254d0c139e404320b
Luispaim <luis.paimadv@gmail.com>
2021-06-21 09:00:50 -0300

    feat(App/Domain): Implementação domain e application

---
+++ src/Avanade.SubTCSE.Projeto.Application/Dtos/EmployeeRole/EmployeeRoleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole
+{
+    public class EmployeeRoleDto
+    {
+        public string Identifcador { get; set; }
+
+        public string Cargo { get; set; }
+    }
+}
