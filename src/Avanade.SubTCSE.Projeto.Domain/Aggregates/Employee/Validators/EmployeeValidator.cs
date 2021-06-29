7dd302212cd71ef2ef5fa8d81ff1af702f14fc41
Luispaim <luis.paimadv@gmail.com>
2021-06-29 08:49:39 -0300

    feat(): Implementação do Employee

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Validators/EmployeeValidator.cs
@@ -0,0 +1,38 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Validators;
+using FluentValidation;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Validators
+{
+    class EmployeeValidator : AbstractValidator<Entities.Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleSet("new", () =>
+            {       
+                RuleFor(e => e.FirstName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.SurName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.Birthday)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.Active)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.Salary)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.EmployeeRole)
+                .SetValidator(new EmployeeRoleValidator());
+
+            });
+        }
+    }
+}
